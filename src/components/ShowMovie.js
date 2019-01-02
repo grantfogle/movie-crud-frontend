48b8abef896fc7b69f0fd79647354277741f57b4
grantfogle <grantjfogle@gmail.com>
2019-01-02 11:25:04 -0700

    edit route is working, neeed to clean up code still

--- src/components/ShowMovie.js
+++ src/components/ShowMovie.js
@@ -5,32 +5,42 @@ class ShowMovie extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            movie: ''
+            id: '',
+            title: '',
+            director: '',
+            year: '',
+            rating: ''
         }
     }
 
     componentDidMount() {
-        //works
         const id = Number(this.props.match.params.id);
+        this.setState({ id })
         if (this.props.movies.map) {
             return this.props.movies.map(movie => {
                 if (movie.id === id) {
-                    return this.setState({ movie })
+                    return (
+                        this.setState({ title: movie.title }),
+                        this.setState({ director: movie.director }),
+                        this.setState({ year: movie.year }),
+                        this.setState({ rating: movie.rating })
+                    )
                 };
             })
         }
     }
 
     handleInput = (event) => {
-        const { value, name } = event.target;
+        const { name, value } = event.target;
         this.setState({ [name]: value })
+        console.log(name, value)
         console.log(this.state)
     }
 
     render() {
         return (
             <div className="">
-                <h2>{this.state.movie.title ? this.state.movie.title : 'cats'}</h2>
+                <h2>{this.state.title ? this.state.title : 'cats'}</h2>
                 <p><a href="this.state.movie.url">Visit imdb page</a></p>
                 <form onSubmit={(e) => {
                     e.preventDefault();
@@ -39,39 +49,43 @@ class ShowMovie extends Component {
                         <label>Movie title</label>
                         <input
                             type="text"
+                            name="title"
                             onChange={(e) => this.handleInput(e)}
-                            defaultValue={this.state.movie.title}
+                            defaultValue={this.state.title}
                         />
                     </div>
                     <div className="form-group">
                         <label>Year</label>
                         <input
                             type="text"
+                            name="year"
                             onChange={(e) => this.handleInput(e)}
-                            defaultValue={this.state.movie.year}
+                            defaultValue={this.state.year}
                         />
                     </div>
                     <div className="form-group">
                         <label>Director</label>
                         <input
                             type="text"
+                            name="director"
                             onChange={(e) => this.handleInput(e)}
-                            defaultValue={this.state.movie.director}
+                            defaultValue={this.state.director}
                         />
                     </div>
                     <div className="form-group">
                         <label>Rating</label>
                         <input
                             type="text"
+                            name="rating"
                             onChange={(e) => this.handleInput(e)}
-                            defaultValue={this.state.movie.rating}
+                            defaultValue={this.state.rating}
                         />
                     </div>
                     <Link to="/">
                         <button
                             className="btn btn-primary"
                             type="submit"
-                            onClick={() => this.props.editMovie(this.state.movie.id, this.state.movie)}>
+                            onClick={() => this.props.editMovie(this.state.id, this.state)}>
                             Edit Movie
                         </button>
                     </Link>
